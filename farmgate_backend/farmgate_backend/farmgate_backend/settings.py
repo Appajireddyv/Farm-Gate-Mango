9c243c0458b79a8f924ebbdfa0b0cb784c5cd5d4
appajireddyv <appajireddyv7@gmail.com>
2026-06-22 15:40:11 +0530

    added csrf origind

--- farmgate_backend/farmgate_backend/farmgate_backend/settings.py
+++ farmgate_backend/farmgate_backend/farmgate_backend/settings.py
@@ -5,9 +5,15 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-farmgate-mango-direct-2026'
 DEBUG = True
-ALLOWED_HOSTS =  ['farmgate-backend.onrender.com',
-                  "localhost"
-                   
+ALLOWED_HOSTS =  [
+    "farmgate-backend.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://farmgate-backend.onrender.com",
+    "https://farmgate-frontend.onrender.com",
 ]
 
 INSTALLED_APPS = [
