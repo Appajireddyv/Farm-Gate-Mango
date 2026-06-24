929c1279d39734521ab523c308ce4c8bd00023cc
appajireddyv <appajireddyv7@gmail.com>
2026-06-24 16:49:06 +0530

    allowed host changes for some sec issues

--- farmgate_backend/farmgate_backend/farmgate_backend/settings.py
+++ farmgate_backend/farmgate_backend/farmgate_backend/settings.py
@@ -5,10 +5,10 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-farmgate-mango-direct-2026'
 DEBUG = True
-ALLOWED_HOSTS =  [
-    "farmgate-backend.onrender.com",
-    "localhost",
-    "127.0.0.1",
+ALLOWED_HOSTS =  ["*"
+    # "farmgate-backend.onrender.com",
+    # "localhost",
+    # "127.0.0.1",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
