a0a14288bee43e416b3339fb03378f72af6c9424
appajireddyv <appajireddyv7@gmail.com>
2026-06-25 16:27:38 +0530

    admin headers and ui changes in urls.py

--- farmgate_backend/farmgate_backend/farmgate_backend/urls.py
+++ farmgate_backend/farmgate_backend/farmgate_backend/urls.py
@@ -3,6 +3,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+admin.site.site_header = "Farm 2 Door Administration"
+admin.site.site_title = "Farm 2 Door Admin"
+admin.site.index_title = "Welcome to Farm 2 Door Dashboard"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('accounts.urls')),
