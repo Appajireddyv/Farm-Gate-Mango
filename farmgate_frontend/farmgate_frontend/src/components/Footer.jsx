c680a3df96dd0c5781ffe56a0f13b58d60fe0413
appajireddyv <appajireddyv7@gmail.com>
2026-07-18 13:09:05 +0530

    updated my old mob number in backend

--- farmgate_frontend/farmgate_frontend/src/components/Footer.jsx
+++ farmgate_frontend/farmgate_frontend/src/components/Footer.jsx
@@ -111,7 +111,7 @@ export default function Footer() {
               </li>
               <li>
                 <Phone size={16} aria-hidden="true" />
-                <a href="tel:+919876543210">+91 74833 24384</a>
+                <a href="tel:+917483324384">+91 7483324384</a>
               </li>
               <li>
                 <MapPin size={16} aria-hidden="true" />
